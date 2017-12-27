0f14ba33c4c5655bdce9eb94bdefc14897d48422
did <2833324528@qq.com>
2017-12-27 19:56:41 +0800

    ...

--- routes/weixin.js
+++ routes/weixin.js
@@ -29,10 +29,42 @@ router.get('/', function (req, res, next) {
 const appId = 'wxfdef306b176c8b93';
 const appsecret = 'b5dae8467401c3175d780db7f81d6ae2';
 
+
+
 router.post('/', function (req, res, next) {
     getAccessToken();
 });
 
+const map =
+    {
+        "button": [
+            {
+                "type": "click",
+                "name": "今日歌曲",
+                "key": "V1001_TODAY_MUSIC"
+            },
+            {
+                "name": "菜单",
+                "sub_button": [
+                    {
+                        "type": "view",
+                        "name": "搜索",
+                        "url": "http://www.soso.com/"
+                    },
+                    {
+                        "type": "miniprogram",
+                        "name": "wxa",
+                        "url": "http://mp.weixin.qq.com",
+                        "appid": "wx286b93c14bbf93aa",
+                        "pagepath": "pages/lunar/index"
+                    },
+                    {
+                        "type": "click",
+                        "name": "赞一下我们",
+                        "key": "V1001_GOOD"
+                    }]
+            }]
+    };
 
 
 function getAccessToken() {
@@ -40,7 +72,16 @@ function getAccessToken() {
         if (err) {
             console.error(err);
         } else {
-            console.log(body);
+            const result = JSON.parse(body);
+            let access_token = result.access_token;
+            request.post({ url: 'https://api.weixin.qq.com/cgi-bin/menu/create?access_token=' + access_token },
+                { form: map }, function (err, httpResponse, body) {
+                    if (err) {
+                        console.error(err);
+                    } else {
+                        console.log(JSON.parse(body));
+                    }
+                });
         }
     });
 }
